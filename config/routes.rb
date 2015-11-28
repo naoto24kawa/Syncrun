d8d48de7629b48f2a3ddd596b10284604984511f
kento_hayasaka <thunders.1028@gmail.com>
2015-11-29 04:28:18 +0900

    add: routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,22 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  # get static#login ログイン画面
+  # post user#login ログイン処理
+  # get static#home HOME画面
+  # get user#show MyPage画面
+  # post user#create ユーザー登録処理
+  # get idea#new Submit画面
+  # post idea#create アイデア登録処理
+  # get idea#show Idea Detail画面
+
+  resources :user, only: [:show, :create]
+  resources :idea, only: [:new, :create, :show]
+
+  get 'login' => 'static#login'
+  post 'login' => 'user#login'
+  get 'home' => 'static#home'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
